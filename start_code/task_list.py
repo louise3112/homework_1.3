c9bcbb505d899b64cc1dc7bab997571bb98f1c4a
Louise Cuthbertson <lcuthbertson_3112@hotmail.co.uk>
2022-11-24 09:13:38 +0000

    updated homework with extensions completed

--- start_code/task_list.py
+++ start_code/task_list.py
@@ -12,68 +12,68 @@ tasks = [
 # 1. Print a list of uncompleted tasks
 print("QUESTION 1")
 
-def uncompleted_tasks():
-    for task in tasks:
+def uncompleted_tasks(list_of_tasks):
+    for task in list_of_tasks:
         if task["completed"] == False:
             print(task)
 
-uncompleted_tasks()
+uncompleted_tasks(tasks)
 
 # 2. Print a list of completed tasks
 print("QUESTION 2")
 
-def completed_tasks():
-    for task in tasks:
+def completed_tasks(list_of_tasks):
+    for task in list_of_tasks:
         if task["completed"] == True:
             print(task)
 
-completed_tasks()
+completed_tasks(tasks)
 
 # 3. Print a list of all task descriptions
 print("QUESTION 3")
 
-def all_tasks():
-    for task in tasks:
+def all_tasks(list_of_tasks):
+    for task in list_of_tasks:
         print(task["description"])
 
-all_tasks()
+all_tasks(tasks)
 
 # 4. Print a list of tasks where time_taken is at least a given time
 print("QUESTION 4")
 
-def tasks_longer_than_time(time):
-    for task in tasks:
+def tasks_longer_than_time(list_of_tasks, time):
+    for task in list_of_tasks:
         if task["time_taken"] >= time:
             print(task)
 
-tasks_longer_than_time(20)
+tasks_longer_than_time(tasks, 20)
 
 # 5. Print any task with a given description
 print("QUESTION 5")
 
-def task_from_description(given_description):
-    for task in tasks:
+def task_from_description(list_of_tasks, given_description):
+    for task in list_of_tasks:
         if task["description"] == given_description:
             print(task)
 
-task_from_description("Feed Cat")
+task_from_description(tasks, "Feed Cat")
 
 # 6. Given a description update that task to mark it as complete.
 print("QUESTION 6")
 
-def update_task_progress(given_description):
-    for task in tasks:
+def update_task_progress(list_of_tasks, given_description):
+    for task in list_of_tasks:
         if task["description"] == given_description:
             task["completed"] = True
 
-update_task_progress("Wash Dishes")
+update_task_progress(tasks, "Wash Dishes")
 print(tasks[0])
 
 # 7. Add a task to the list
 print("QUESTION 7")
 
-def add_task(task_description, status, task_length):
-    tasks.append(
+def add_task(list_of_tasks, task_description, status, task_length):
+    list_of_tasks.append(
         {
             "description": task_description,
             "completed": status,
@@ -81,43 +81,56 @@ def add_task(task_description, status, task_length):
         }
     )
 
-add_task("Hoover", False, 25)
+add_task(tasks, "Hoover", False, 25)
 print(tasks)
 
 #8. Use a while loop to display the following menu and allow the user to enter an option.
 #9. Call the appropriate function depending on the users choice.
-# print("QUESTIONS 8 & 9")
+print("QUESTIONS 8 & 9")
 
-# def task_menu():
+def task_menu(list_of_tasks):
 
-#     user_input = "m"
+    user_input = ""
 
-#     while user_input.lower() == "m":
-#         print("Menu:")
-#         print("1: Display all tasks")
-#         print("2: Display uncompleted tasks")
-#         print("3: Display completed tasks")
-#         print("4: Mark task as complete")
-#         print("5: Get tasks which take longer than a given time")
-#         print("6: Find task by description")
-#         print("7. Add a new task to list")
-#         print("M or m: Display this menu")
-#         print("Q or q: Quite")
+    while user_input == "":
+        print("Menu:")
+        print("1: Display all tasks")
+        print("2: Display uncompleted tasks")
+        print("3: Display completed tasks")
+        print("4: Mark task as complete")
+        print("5: Get tasks which take longer than a given time")
+        print("6: Find task by description")
+        print("7. Add a new task to list")
+        print("M or m: Display this menu")
+        print("Q or q: Quit")
 
-#         user_input = input("Please select the relevant menu item: ")
-#         if user_input == "1":
-#             #list of all tasks
-#         elif user_input == "2":
-#             uncompleted_tasks()
-#         elif user_input == "3":
-#             completed_tasks()
-#         elif user_input == "4":
-#             update_task_progress() #NEED TO ADD WAY OF ENTERING TASK DESCRIPTION
-#         elif user_input == "5":
-#             tasks_longer_than_time() #NEED TO ADD WAY OF ENTERING TIME
-#         elif user_input == "6":
-#             task_from_description()  #NEED TO ADD WAY OF ENTERING TASK DESCRIPTION
-#         elif user_input == "7":
-#             add_task()  #NEED TO ADD WAY OF ENTERING TASK INFO
+        user_input = input("Please select the relevant menu item: ")
 
-# task_menu()
+        if user_input == "1":
+            all_tasks(list_of_tasks)
+        elif user_input == "2":
+            uncompleted_tasks(list_of_tasks)
+        elif user_input == "3":
+            completed_tasks(list_of_tasks)
+        elif user_input == "4":
+            completed_task = input("Please enter the description of the task you would like to mark as completed: ")
+            update_task_progress(list_of_tasks, completed_task)
+        elif user_input == "5":
+            min_time = input("Please enter the minimum length of time for the task: ")
+            tasks_longer_than_time(list_of_tasks, int(min_time))
+        elif user_input == "6":
+            task_description = input("Please enter the description for the task: ")
+            task_from_description(list_of_tasks, task_description) 
+        elif user_input == "7":
+            new_description = input("Please enter a description for the task to be added: ")
+            new_status = input("Please enter the status of the task to be added: ")
+            new_time = input("Please enter the time required for the task to be added: ")
+            add_task(list_of_tasks, new_description, new_status, new_time)
+        elif user_input.lower() == "m":
+            task_menu(list_of_tasks)
+        elif user_input.lower() == "q":
+            break
+        else:
+            user_input = ""
+
+task_menu(tasks)
